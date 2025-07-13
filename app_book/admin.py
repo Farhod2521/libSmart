211d558104e613f4bb7842c8509eadb9ee757516
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-07-13 13:33:57 +0500

    fixed

--- app_book/admin.py
+++ app_book/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from modeltranslation.admin import TranslationAdmin
 from .models import Book, CategoryBook
+
+from modeltranslation.admin import TranslationAdmin
 
 
 @admin.register(CategoryBook)
