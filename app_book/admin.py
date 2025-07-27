988134f7375a1e0320a91633a9b741b5a2ec8585
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-07-27 13:20:32 +0500

    fixed

--- app_book/admin.py
+++ app_book/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book, CategoryBook
+from .models import Book, CategoryBook, SearchHistory
 
 from modeltranslation.admin import TranslationAdmin
 
@@ -17,3 +17,18 @@ class BookAdmin(TranslationAdmin):
     search_fields = ('title', 'creator', 'publisher', 'subject')
     list_filter = ('language', 'date', 'type')
     ordering = ('id',)
+
+
+@admin.register(SearchHistory)
+class SearchHistoryAdmin(admin.ModelAdmin):
+    list_display = ('query', 'customer_display', 'book', 'searched_at')
+    list_filter = ('searched_at',)
+    search_fields = ('query', 'customer__user__full_name', 'book__title')
+    autocomplete_fields = ('customer', 'book')
+    ordering = ['-searched_at']
+
+    def customer_display(self, obj):
+        if obj.customer:
+            return obj.customer.user.full_name
+        return "Anonim"
+    customer_display.short_description = "Foydalanuvchi"
