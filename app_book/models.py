d1f199bab5d93c8a508fb321be2f7d3438278baf
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-07-27 12:59:58 +0500

    fixed

--- app_book/models.py
+++ app_book/models.py
@@ -96,7 +96,7 @@ from django.utils import timezone
 
 class SearchHistory(models.Model):
     customer = models.ForeignKey(
-        Customer, on_delete=models.SET_NULL,
+        "app_user.Customer", on_delete=models.SET_NULL,
         null=True, blank=True, related_name='search_histories',
         verbose_name="Foydalanuvchi"
     )
